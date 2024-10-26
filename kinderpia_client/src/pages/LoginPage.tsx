63722e7fe13bf47ce55bce4821525b1a08e49603
yena <spffhd0835@gmail.com>
2024-10-26 23:48:06 +0900

    Add: 로그인인풋 컴포넌트 적용 및 키보드및 스크린 리더 접근성 코드 추가

--- kinderpia_client/src/pages/LoginPage.tsx
+++ kinderpia_client/src/pages/LoginPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/domain/LoginPage.scss';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import LoginInput from '../components/domain/LoginInput';
 
 interface LoginFormInputs {
   userId: string;
@@ -30,96 +31,58 @@ export default function LoginPage() {
     });
   };
 
+  // 해당 필드를 빈 문자열로 설정
   const clearInput = (field: 'userId' | 'userPw') => {
-    setValue(field, ''); // 해당 필드를 빈 문자열로 설정
+    setValue(field, '');
   };
-
-  // 로그인버튼눌렀을때
-  // 아이디 없으면 아이디를 입력해주세요 ok
-  // 비밀번호 없으면 비밀번호를 입력해주세요 ok
-  //두개 다 틀리면 아이디 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.-> 서버의 user테이블에 해당정보가 있는지 확인후
 
   const onSubmit: SubmitHandler<LoginFormInputs> = (data) => {
     console.log('로그인 데이터:', data);
+    //두개 다 틀리면 아이디 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.-> 서버의 user테이블에 해당정보가 있는지 확인후
   };
 
   return (
     <div className="login-container">
       <h2 className="logo">LOGIN</h2>
       <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="input-wrap">
-          <label htmlFor="userId" className="label">
-            아이디
-          </label>
-          <input
-            type="text"
-            id="userId"
-            {...register('userId', { required: '아이디를 입력해주세요.' })}
-            onFocus={(e) => {
-              const label = e.target.previousElementSibling;
-              if (label && !e.target.value) {
-                label.classList.add('shrink');
-              }
-            }}
-            onBlur={(e) => {
-              const label = e.target.previousElementSibling;
-              if (label && !e.target.value) {
-                label.classList.remove('shrink');
-              }
-            }}
-          />
-          <span
-            className="xi-close-circle eraser-icon"
-            onClick={() => clearInput('userId')}
-          ></span>
-        </div>
-        <div className="input-wrap">
-          <label htmlFor="userPw" className="label">
-            비밀번호
-          </label>
-          <input
-            type={showPassword ? 'text' : 'password'}
-            id="userPw"
-            {...register('userPw', { required: '비밀번호를 입력해주세요.' })}
-            onFocus={(e) => {
-              const label = e.target.previousElementSibling;
-              if (label && !e.target.value) {
-                label.classList.add('shrink');
-              }
-            }}
-            onBlur={(e) => {
-              const label = e.target.previousElementSibling;
-              if (label && !e.target.value) {
-                label.classList.remove('shrink');
-              }
-            }}
-          />
-          <span
-            className="xi-close-circle eraser-icon"
-            onClick={() => clearInput('userPw')}
-          ></span>
-          <span
-            className={`${eyeIconClass} eye-icon`}
-            onClick={togglePasswordVisibility}
-          ></span>
-          <div className="errmsg-box">
-            {errors.userId && (
-              <span className="err-msg">{errors.userId.message}</span>
-            )}
-            {errors.userPw && (
-              <span className="err-msg">{errors.userPw.message}</span>
-            )}
-          </div>
-        </div>
+        <LoginInput
+          label="아이디"
+          type="text"
+          id="userId"
+          register={register}
+          requiredMessage="아이디를 입력해주세요."
+          clearInput={() => clearInput('userId')}
+          error={errors.userId?.message}
+        />
+        <LoginInput
+          label="비밀번호"
+          type={showPassword ? 'text' : 'password'}
+          id="userPw"
+          register={register}
+          requiredMessage="비밀번호를 입력해주세요."
+          clearInput={() => clearInput('userPw')}
+          error={errors.userPw?.message}
+          showPasswordToggle={togglePasswordVisibility}
+          eyeIconClass={eyeIconClass}
+          isPassword={true}
+        />
         <button type="submit" className="login-btn">
           로그인
         </button>
       </form>
       <div className="link-wrap">
-        <Link to={'/user/login'} className="register-link">
+        <Link
+          to={'/user/login'}
+          className="register-link"
+          aria-label="회원가입 페이지로 이동"
+        >
           회원가입
         </Link>
-        <a href="https://www.naver.com" className="admin-link">
+        <a
+          href="https://www.naver.com"
+          className="admin-link"
+          aria-label="관리자 로그인 페이지로 이동"
+        >
           관리자 로그인
         </a>
       </div>
