0b04a7cf8765935ba5a75ab23e26212ce8ce986e
yena <spffhd0835@gmail.com>
2024-10-26 18:46:49 +0900

    Add: 로그인페이지 라우터 추가

--- kinderpia_client/src/App.tsx
+++ kinderpia_client/src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './layout/Layout';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            {/* 로그인 */}
+            <Route path="/user/login" element={<LoginPage />} />
+            {/* 회원가입 */}
+            {/* 마이페이지 */}
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
