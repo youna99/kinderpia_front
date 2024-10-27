3a4c980215d2e817c7ae2b0622fc137205b5412e
youna <dbsk0108@gmail.com>
2024-10-28 00:29:24 +0900

    Add:  PlaceLists 페이지 라우터 설정

--- kinderpia_client/src/App.tsx
+++ kinderpia_client/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './layout/Layout';
+import PlaceLists from './pages/PlaceLists';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route path="placelist" element={<PlaceLists />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
