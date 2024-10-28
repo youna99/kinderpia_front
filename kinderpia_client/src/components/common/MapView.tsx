fc6f584a585c10e2f9667b08920a749522bed22c
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-10-29 05:43:24 +0900

    feat : 지도 컴포넌트 작업

--- kinderpia_client/src/components/common/MapView.tsx
+++ kinderpia_client/src/components/common/MapView.tsx
@@ -1,24 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from 'react'
 interface MapViewProps {
   latitute: number;
-  longitute: number;
+  longitude: number;
 }
 
 const MapView:React.FC<MapViewProps> = ({
-  latitute, 
-  longitute,
+  latitute = 127.0602549, 
+  longitude = 37.2874971,
 }) => {
+  const [src , setSrc] = useState('');
   useEffect(() => {
-  }, []);
+    setSrc(`https://simg.pstatic.net/static.map/v2/map/staticmap.bin?crs=EPSG:4326&baselayer=bl_vc_bg&overlayers=ol_vc_an&scale=2&caller=mw_smart_booking&overlayers=ol_vc_an&center=${longitude},${latitute}&markers=color:0x59A4D6|type:c|size:mid|label:a|pos:${longitude} ${latitute}&level=14&w=335&h=170&lang=ko`);
+  }, [ latitute, longitude ]);
   return ( // 지도는 임시지도입니다!
-    <img 
-      className="map" 
-      src="https://simg.pstatic.net/static.map/v2/map/staticmap.bin?crs=EPSG:4326&baselayer=bl_vc_bg&overlayers=ol_vc_an&scale=2&caller=mw_smart_booking&overlayers=ol_vc_an&center=127.0602549,37.2874971&markers=type:c|size:mid|label:cafe|pos:127.0602549 37.2874971&level=16&w=335&h=170&lang=ko" 
-      width="100%" 
-      height="100%" 
-      alt="map"
-    />
+    <div>
+      <img 
+        className="map" 
+        src={src} 
+        width="100%" 
+        height="100%" 
+        alt="map"
+      />
+    </div>
   );
 }
 
