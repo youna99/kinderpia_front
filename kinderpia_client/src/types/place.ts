fd04b874ea3e72153a578a131cee0fa73417056e
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-10-29 05:43:11 +0900

    feat : 장소 페이지 작업

--- kinderpia_client/src/types/place.ts
+++ kinderpia_client/src/types/place.ts
@@ -4,11 +4,19 @@ export interface PlaceData{
   category: string;
   location: string;
   latitute :number;
-  longitute :number;
+  longitude :number;
   description: string;
   img : string;
   payment : string;
   openTime : string;
   webPageUrl : string;
   phone : string;
+}
+
+export interface ReviewData{
+  id : number;
+  writer: string;
+  content : string;
+  star : number;
+  createdAt : string;
 }
