fa99769f0571504acf9d86dfe53b718ea7e477c4
DAIN302 <dbgnfk12@gmail.com>
2024-10-29 01:21:32 +0900

    add : 채팅방 UI

--- kinderpia_client/src/components/chat/ChatContainer.tsx
+++ kinderpia_client/src/components/chat/ChatContainer.tsx
@@ -1,16 +1,20 @@
-import '../../styles/chat/ChatContainer.scss'
-import ChatMessage from './ChatMessage'
+import "../../styles/chat/ChatContainer.scss";
+import ChatMessage from "./ChatMessage";
 
 // 채팅방 메시지 컨테이너 컴포넌트
 export default function ChatContainer() {
-  // 내가 보낸 메시지, 다른 사람이 보낸 메시지 구분 필요 -> sender로 하기
+  // 내가 보낸 메시지, 다른 사람이 보낸 메시지 구분 필요 -> sender 로 하고 나의 메시지는 own 으로 표시
+  // 메시지 -> msg, 메시지 시간 -> time, 프사 -> img, 닉네임 -> sender
+
   return (
-    <div className='chat-container'>
-      <ChatMessage sender={'own'}/>
-      <ChatMessage sender={'other'}/>
-      <ChatMessage sender={'own'}/>
-      <ChatMessage sender={'other'}/>
-      <ChatMessage sender={'own'}/>
+    <div className="chat-container">
+      <ChatMessage
+        sender="슈슈슈슈퍼노바"
+        msg="사건은 다가와 아오에 거세게 다가와 아오에 감히 건드리지 못할걸 누구든 말야 지금 내안에선 슈슈슈슈퍼노바 노바 하이퍼 캔스탑 스텔라 원초 그걸 찾아 아파트아파트 아파트아파트 아파트"
+        time="09:11"
+        img="https://dain302.github.io/kokoa-clone-2022/photo/cherrycookie.jpg"
+      />
+      <ChatMessage sender="own" msg="hello" time="09:11" img="dkdkdkdk" />
     </div>
-  )
+  );
 }
--- kinderpia_client/src/components/chat/ChatMessage.tsx
+++ kinderpia_client/src/components/chat/ChatMessage.tsx
@@ -2,22 +2,33 @@ import "../../styles/chat/ChatMessage.scss";
 
 interface ChatMessageProp {
   sender: string;
+  msg: string;
+  time: string;
+  img: string;
 }
 
 // 채팅방 메시지 컴포넌트 : 내가 보낸 메시지와 타인의 메시지 구분 필요 -> 일단 className 으로 구분
 // -> 부모로부터 props 로 전달받기
-export default function ChatMessage({ sender }: ChatMessageProp) {
+export default function ChatMessage({
+  sender,
+  msg,
+  time,
+  img,
+}: ChatMessageProp) {
   return (
-    <div className={`message message-${sender}`}>
-      {sender === "other" ? (
+    <div className={`message message-${sender === "own" ? "own" : "other"}`}>
+      {sender !== "own" ? (
         <figure className="message-profile">
-          <img />
+          <img src={img} alt="profile image" />
         </figure>
       ) : null}
       <div className="message-content">
+        {sender !== "own" ? (
+          <div className="message-sender">{sender}</div>
+        ) : null}
         <div className="message-info">
-          <span className="message-bubble"></span>
-          <span className="message-time"></span>
+          <span className="message-bubble">{msg}</span>
+          <span className="message-time">{time}</span>
         </div>
       </div>
     </div>
