2e4c3d459a078de92114b19de1ee3bc9f3d08291
yenaf <spffhd0835@gmail.com>
2024-10-28 13:53:45 +0900

    add: 머징 후 라우터 추가 및 중복코드 제거

--- kinderpia_client/src/App.tsx
+++ kinderpia_client/src/App.tsx
@@ -4,13 +4,9 @@ import { Provider } from 'react-redux';
 import Layout from './layout/Layout';
 import ChatlistPage from './pages/ChatListPage';
 import store from './store';
-import React from 'react';
 import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Layout from './layout/Layout';
 import LoginPage from './pages/LoginPage';
 import PlaceLists from './pages/PlaceLists';
-
 import Meeting from './pages/meeting/Meeting';
 import MeetingCreate from './pages/meeting/MeetingCreate';
 import MeetingDetail from './pages/meeting/MeetingDetail';
