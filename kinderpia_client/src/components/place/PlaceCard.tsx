acbf67394ea71e8c94c0e7d3eb30b736178ab2e5
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-10-28 18:47:47 +0900

    fix : 장소 페이지 구현 - 1

--- kinderpia_client/src/components/place/PlaceCard.tsx
+++ kinderpia_client/src/components/place/PlaceCard.tsx
@@ -1,6 +1,36 @@
 import React from 'react'
+interface PlaceCardProps{
+  id : number;
+  title: string;
+  category: string;
+  location: string;
+  latitute :number;
+  longitute :number;
+  description: string;
+  img : string;
+  payment : string;
+  openTime : string;
+  webPageUrl : string;
+  phone : string;
+  onClick: (value: string) => void;
+}
 
-function PlaceCard() {
+
+const PlaceCard: React.FC<PlaceCardProps> = ({
+  id,
+  title,
+  category,
+  location,
+  latitute,
+  longitute,
+  description,
+  img,
+  payment,
+  openTime,
+  webPageUrl,
+  phone,
+  onClick
+}) => {
   return (
     <div>PlaceCard</div>
   )
