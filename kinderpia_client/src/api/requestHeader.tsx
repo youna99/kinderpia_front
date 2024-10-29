a9c204918710feef68d0a3e44df168389cc756c4
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-10-30 01:24:54 +0900

    feat : 오타수정

--- kinderpia_client/src/api/requestHeader.tsx
+++ kinderpia_client/src/api/requestHeader.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const requsetHeader = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000', // 서버 주소로 변경 필요함
+export const requestHeader = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
