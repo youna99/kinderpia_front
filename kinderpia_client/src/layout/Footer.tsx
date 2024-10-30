0e108e6aa7df461b628a437a5d2c423a70199fcd
yena <spffhd0835@gmail.com>
2024-10-31 02:34:04 +0900

    design: 헤더, 푸터, 네이게이션바, 메뉴바 디자인구현

--- kinderpia_client/src/layout/Footer.tsx
+++ kinderpia_client/src/layout/Footer.tsx
@@ -4,7 +4,19 @@ import '../styles/common/Footer.scss';
 export default function Footer() {
   return (
     <footer>
-      <div></div>
+      <div className="inner">
+        <div className="footer-info">
+          <a href="https://github.com/SeSAC-3rd-Kinderpia">
+            <span className="xi-github-alt"></span>
+            <span className="footer-infotxt">github</span>
+          </a>
+        </div>
+        <p className="copyright">
+          &copy;
+          <span className="this-year">{new Date().getFullYear()}</span>
+          킨더피아
+        </p>
+      </div>
     </footer>
   );
 }
--- kinderpia_client/src/layout/Header.tsx
+++ kinderpia_client/src/layout/Header.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import '../styles/common/Header.scss';
 import { Link } from 'react-router-dom';
+import Footer from './Footer';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
   };
   return (
     <>
@@ -15,24 +19,57 @@ export default function Header() {
           <h1>
             <Link to={'/'}>KINDERPIA</Link>
           </h1>
-          <button className="xi-bars nav-icon" onClick={toggleMenu}></button>
-          <nav className="dropdown-menu">
+          <button
+            className={`nav-icon ${isMenuOpen ? 'xi-close' : 'xi-bars'}`}
+            onClick={toggleMenu}
+          ></button>
+          <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
+            <strong>MENU</strong>
             {isMenuOpen && (
               <ul className="header-nav">
                 <li className="header-list">
-                  <Link to={'/login'}>로그인</Link>
+                  <Link to={'/meeting'} onClick={handleLinkClick}>
+                    모임 찾기
+                  </Link>
                 </li>
                 <li className="header-list">
-                  <Link to={'/signup'}>회원가입</Link>
+                  <Link to={'/place'} onClick={handleLinkClick}>
+                    장소 찾기
+                  </Link>
                 </li>
                 <li className="header-list">
-                  <Link to={'/meeting'}>모임 찾기</Link>
+                  <Link
+                    to={'/user/login'}
+                    className="login-btn"
+                    onClick={handleLinkClick}
+                  >
+                    LOGIN
+                  </Link>
                 </li>
                 <li className="header-list">
-                  <Link to={'/place'}>장소 찾기</Link>
+                  <Link
+                    to={'user/register'}
+                    className="register-btn"
+                    onClick={handleLinkClick}
+                  >
+                    회원가입
+                  </Link>
                 </li>
               </ul>
             )}
+            <div className="project-info inner">
+              <div className="footer-info">
+                <a href="https://github.com/SeSAC-3rd-Kinderpia">
+                  <span className="xi-github-alt"></span>
+                  <span className="footer-infotxt">github</span>
+                </a>
+              </div>
+              <p className="copyright">
+                &copy;
+                <span className="this-year">{new Date().getFullYear()}</span>
+                킨더피아
+              </p>
+            </div>
           </nav>
         </div>
       </header>
