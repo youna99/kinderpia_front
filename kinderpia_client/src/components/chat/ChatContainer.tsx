bda85515c6b456db7b356cf42e2fe1065c82e8a3
DAIN302 <dbgnfk12@gmail.com>
2024-10-31 02:49:15 +0900

    feat : 웹 소켓 초기 설정 입력 필드 설정 필요

--- kinderpia_client/src/components/chat/ChatContainer.tsx
+++ kinderpia_client/src/components/chat/ChatContainer.tsx
@@ -2,32 +2,63 @@ import { useEffect, useState } from "react";
 import "../../styles/chat/ChatContainer.scss";
 import ChatMessage from "./ChatMessage";
 import { Client } from "@stomp/stompjs";
-import SockJS from 'sockjs-client';
-
+import SockJS from "sockjs-client";
 
 interface ChatRoomProps {
   chatroomId: number;
+}
+
+interface ChatMessageType {
+  chatmsgId: number;
+  chatroomId: number;
+  senderId: number;
+  senderNickname: string;
+  senderProfileImg: string;
+  chatmsgContent: string;
+  createdAt: string; // LocalDateTime 형식의 문자열
+  messageType: string; // MessageType의 문자열 표현
 }
 
 // 채팅방 메시지 컨테이너 컴포넌트
 export default function ChatContainer({ chatroomId }: ChatRoomProps) {
   // 내가 보낸 메시지, 다른 사람이 보낸 메시지 구분 필요 -> sender 로 하고 나의 메시지는 own 으로 표시
   // 메시지 -> msg, 메시지 시간 -> time, 프사 -> img, 닉네임 -> sender
-  
-  // 경로 
-  const url = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
+  // 경로
+  const url = process.env.REACT_APP_API_URL || "http://localhost:3000";
 
   // 메세지
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<ChatMessageType[]>([]);
 
   // 소켓 설정
   useEffect(() => {
-    const socket =  new SockJS(`${url}/topic/chatroom/${chatroomId}`);
+    const socket = new SockJS(`${url}/topic/chatroom/${chatroomId}`);
 
     const client = new Client({
-      webSocketFactory : () => socket
+      webSocketFactory: () => socket,
+      debug: (str) => {
+        console.log(str);
+      },
+      onConnect: () => {
+        // 채팅방 메시지 구독
+        client.subscribe(`/topic/chatroom/${chatroomId}`, (message) => {
+          const chatMessage = JSON.parse(message.body);
+          setMessages((prevMessages) => [...prevMessages, chatMessage]);
+        })
+      },
+      onStompError: (frame) => {
+        console.error(frame.body);
+      }
     });
-  }, []);
+
+    client.activate;
+
+
+    // 언마운트 시 소켓 연결 종료
+    return () => {
+      client.deactivate;
+    }
+  }, [chatroomId]);
 
   return (
     <div className="chat-container">
