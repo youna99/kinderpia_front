d9d6ab9e33dce6cf61a55155866e81028d7c5cd7
DAIN302 <dbgnfk12@gmail.com>
2024-11-01 14:53:32 +0900

    add: env 추가

--- kinderpia_client/src/api/chat.ts
+++ kinderpia_client/src/api/chat.ts
@@ -10,21 +10,9 @@ export const getChatRoom = (chatroomId: number) => {
   requestHeader.get(`/api/chatroom/${chatroomId}`);
 };
 
-// 채팅 메세지 전송
-export const sendChatMessage = (chatroomId:number) => {
-    requestHeader.post(`/api/${chatroomId}/chatmsg`)
-};
-
-// 채팅 메세지 수신 경로(WebSocket 전용) -> 이건 좀 보기
-// stomp/js 이거랑 socket-client 이거 필요 (라이브러리))
 
 // 채팅 메시지 조회(목록)
 export const getChatMessages = (chatroomId:number) => {
     requestHeader.get(`/api/chatroom/chatmsg/${chatroomId}`)
 };
 
-
-// 채팅 메세지 신고
-export const reportMessage = () => {
-    requestHeader.post(`/api/report/chatmsg`)
-};
--- kinderpia_client/src/components/chat/ChatReport.tsx
+++ kinderpia_client/src/components/chat/ChatReport.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef } from "react";
-import { reportMessage } from "../../api/chat"
 
 // 채팅 신고 컴포넌트 -> 상대방의 메시지를 누르거나 멤버 목록에서 상대 누르면 나옴
 export default function ChatReport() {
   const reportRef = useRef<HTMLDivElement>(null);
   
   const handleReport = async () => {
-    const res = await reportMessage();
+    
   }
 
   useEffect(()=> {
