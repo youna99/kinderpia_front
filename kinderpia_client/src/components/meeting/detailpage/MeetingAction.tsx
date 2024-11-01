7d107ccd149ec263ad0a7cebe4463e1cc6529bd8
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-11-01 17:45:30 +0900

    feat : 미팅 상세 페이지 구조

--- kinderpia_client/src/components/meeting/detailpage/MeetingAction.tsx
+++ kinderpia_client/src/components/meeting/detailpage/MeetingAction.tsx
@@ -1,17 +1,34 @@
 import React from 'react'
 
-//타입 호출
+// 타입 호출
 import { MeetingUserData } from '../../../types/meeting'
 
-interface MeetingActionProps{
-  data? : MeetingUserData
+// 컴포넌트 호출
+import MeetingActionJoin from './MeetingActionJoin'
+import MeetingActionJoined from './MeetingActionJoined'
+import MeetingActionWait from './MeetingActionWait'
+
+// 스타일 호출
+import '../../../styles/meeting/detailpage/MeetingAction.scss'
+
+interface MeetingActionProps {
+  data?: MeetingUserData
 }
 
-const MeetingAction:React.FC<MeetingActionProps> = ({
+const MeetingAction: React.FC<MeetingActionProps> = ({
   data
 }) => {
+
+  const meetingActionJoinReq= ()=>{
+
+  }
   return (
     <div className='meeting-action-container'>
+      {!data?.isJoined && <MeetingActionJoin
+        onSubmit={meetingActionJoinReq}
+      />}
+      {data?.isJoined && !data?.ispermitted && <MeetingActionWait />}
+      {data?.isJoined && data?.ispermitted && <MeetingActionJoined />}
     </div>
   )
 }
