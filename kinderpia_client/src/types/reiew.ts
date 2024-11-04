d944c3ee985678f377d20e83023307779f65fe4f
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-11-04 14:30:39 +0900

    feat : 아아아아

--- kinderpia_client/src/types/reiew.ts
+++ kinderpia_client/src/types/reiew.ts
@@ -1,15 +1,15 @@
-
 // types/review.ts
+
+// 리뷰 작성/수정 시 사용하는 DTO
 export interface ReviewFormDTO {
-  reviewId?: number;  // 수정 시에만 필요
+  reviewId?: number;
   placeId: number;
   userId: number;
   star: number;
   reviewContent: string;
 }
 
-
-// types/review.ts
+// 개별 리뷰 아이템의 기본 정보
 export interface ReviewItem {
   reviewId: number;
   star: number;
@@ -19,21 +19,21 @@ export interface ReviewItem {
   deleted: boolean;
 }
 
-export interface ReviewData {
+// 리뷰 데이터 전체 구조
+export interface Review {
   review: ReviewItem;
   nickname: string;
-  profileImg: string;
+  profileImg: string | null;
   likeCount: number;
   blacklist: boolean;
 }
 
-export interface ReviewResponseData {
-  reviews: ReviewData[];
-  averageStar: number;
-}
-
-export interface ReviewResponse {
-  data: ReviewResponseData;
-  status: number;
-  message: string;
+// API 응답 구조
+export interface ReviewsResponse {
+    data: {
+      reviews: Review[];
+      averageStar: number;
+    };
+    status: number;
+    message: string;
 }
