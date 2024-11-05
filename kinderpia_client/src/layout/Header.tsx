17cdaf322353e8ce1cd6ea049cb427409f079c10
yena <spffhd0835@gmail.com>
2024-11-05 15:35:52 +0900

    add: 로그아웃시 메인페이지로 이동하는 코드추가

--- kinderpia_client/src/layout/Header.tsx
+++ kinderpia_client/src/layout/Header.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/common/Header.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { extractUserIdFromCookie } from '../utils/extractUserIdFromCookie';
 import { confirmAlert, simpleAlert } from '../utils/alert';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [userIdFromCookie, setUserIdFromCookie] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -45,7 +46,8 @@ export default function Header() {
     if (confirmed) {
       // 쿠키삭제
       document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-      simpleAlert('success', '로그아웃 되었습니다.', 'top-end');
+      await simpleAlert('success', '로그아웃 되었습니다.', 'top-end');
+      navigate('/');
     }
     return;
   };
