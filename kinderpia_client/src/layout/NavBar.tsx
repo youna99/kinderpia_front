5408f2fd1c6b444714faf7b4ae9821928515952c
DAIN302 <dbgnfk12@gmail.com>
2024-11-06 19:46:45 +0900

    fix : 채팅방 메세지에 관련된 버그 수정?

--- kinderpia_client/src/layout/NavBar.tsx
+++ kinderpia_client/src/layout/NavBar.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import '../styles/common/NavBar.scss';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
 
 export default function NavBar() {
+  const {unreadCounts, unreadMessages} = useSelector((state:RootState) => state.chat)
+  console.log('안읽은메시지',Object.keys(unreadCounts), unreadMessages);
   return (
     <nav className="navigation">
       <ul className="nav-lists">
@@ -21,6 +25,7 @@ export default function NavBar() {
         <li className="nav-list">
           <Link to={'/chatroom/list'}>
             <span className="xi-message-o nav-icon"></span>
+            {/* <span className={Object.keys(unreadCounts).length > 0 ? `chat-badge`: ''}></span> */}
             <span>채팅</span>
           </Link>
         </li>
