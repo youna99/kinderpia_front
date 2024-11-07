fc4cdabfdbf26d225d152c49f2fab922a0a51a9e
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-11-08 02:29:23 +0900

    feat : 수정

--- map_server/index.js
+++ map_server/index.js
@@ -7,15 +7,66 @@ require('dotenv').config();
 console.log('.env 는 노션 Kinderpia 메인 페이지 제~일 밑에');
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
+const allowedOrigins = [
+  // 개발 환경
+  'http://localhost:3000',  // React 클라이언트 개발 서버
+  'http://localhost:3001',  // React 어드민 개발 서버
+  
+  // 프로덕션 환경
+  'http://ec2-3-38-150-41.ap-northeast-2.compute.amazonaws.com',
+  'https://ec2-3-38-150-41.ap-northeast-2.compute.amazonaws.com',
+  'http://ec2-3-38-150-41.ap-northeast-2.compute.amazonaws.com:3000',  // 클라이언트
+  'http://ec2-3-38-150-41.ap-northeast-2.compute.amazonaws.com:3001',  // 어드민
+  
+  // 도메인이 있는 경우
+  'http://kinderpia.com',
+  'https://kinderpia.com',
+  'http://admin.kinderpia.com',
+  'https://admin.kinderpia.com',
+  'http://www.kinderpia.com',
+  'https://www.kinderpia.com'
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    // development 환경에서의 API 테스트 도구 허용 (Postman 등)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Not allowed by CORS: ${origin}`));
+    }
+  },
+  methods: ['GET', 'POST'],  // 맵 서버는 검색 기능만 제공하므로 GET, POST만 허용
+  credentials: true,  // 인증 정보 허용
+  maxAge: 86400,     // preflight 요청 캐시 24시간
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'Accept',
+    'Origin'
+  ]
+};
+
+app.use(cors(corsOptions));
+
+// CORS 에러 핸들링 미들웨어
+app.use((err, req, res, next) => {
+  if (err.message.startsWith('Not allowed by CORS')) {
+    console.error('CORS Error:', err.message);
+    res.status(403).json({
+      error: 'CORS Error',
+      message: '허용되지 않은 출처입니다.',
+      origin: req.headers.origin
+    });
+  } else {
+    next(err);
+  }
+});
 
 app.use(express.json());
-
-let savedLocation = null;
 
 app.get('/map/api/search', async (req, res) => {
   const { query } = req.query;
