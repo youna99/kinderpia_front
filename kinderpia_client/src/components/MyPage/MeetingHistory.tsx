2f67a430213f66c15d8369e563a8618780a966e1
yenaf <spffhd0835@gmail.com>
2024-11-07 15:38:00 +0900

    add:내모임, 내가만든모임까지는 페이지네이션 가능

--- kinderpia_client/src/components/MyPage/MeetingHistory.tsx
+++ kinderpia_client/src/components/MyPage/MeetingHistory.tsx
@@ -4,6 +4,7 @@ import '../../styles/mypage/MeetingHistory.scss';
 import { formatDetailDate } from '../../utils/formatDate';
 import { requestHeader } from '../../api/requestHeader';
 import { MyInfoProps } from '../../types/user';
+
 export interface MettingListInfo {
   capacity: number;
   createdAt: string;
@@ -19,65 +20,93 @@ export interface MettingListInfo {
   profileImg: string;
 }
 
+interface MeetingResponse {
+  data: {
+    dataList: MettingListInfo[];
+    pageInfo: {
+      page: number;
+      totalPages: number;
+      // 필요한 다른 페이지 정보가 있으면 여기에 추가
+    };
+  };
+}
+
 const MeetingHistory: React.FC<MyInfoProps> = ({ userInfo }) => {
   const [meetings, setMeetings] = useState<MettingListInfo[]>([]);
   const [filter, setFilter] = useState<string>('all');
   const [page, setPage] = useState(1); // 현재 페이지 상태
   const [totalPages, setTotalPages] = useState(1); // 총 페이지 수 상태
+  const [loading, setLoading] = useState(false); // 로딩 상태
 
   useEffect(() => {
-    const fetchMeetings = async () => {
-      if (!userInfo) return;
+    fetchMeetings();
+  }, [filter]);
 
-      try {
-        let response;
-        switch (filter) {
-          case 'all':
-            response = await fetchAllMeetings(page);
-            break;
-          case 'created':
-            response = await fetchCreatedMeetings(page);
-            break;
-          case 'ongoing':
-            response = await fetchOngoingMeetings(page);
-            break;
-          default:
-            break;
-        }
-
-        if (response) {
-          setMeetings(response);
-        }
-      } catch (error) {
-        console.error('모임 목록 로드 중 오류 발생:', error);
+  useEffect(() => {
+    const handleScroll = () => {
+      const bottom =
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 100;
+      if (bottom && !loading) {
+        fetchMeetings(); // 스크롤이 바닥에 도달했을 때 데이터 로드
       }
     };
 
-    fetchMeetings();
-  }, [userInfo, filter, page]);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loading]);
 
-  const fetchAllMeetings = async (page: number) => {
+  const fetchMeetings = async () => {
+    if (loading || page > totalPages) return; // 로딩 중이거나 페이지가 초과하면 종료
+    setLoading(true);
+
+    try {
+      let response: MeetingResponse;
+      switch (filter) {
+        case 'all':
+          response = await fetchAllMeetings(page);
+          break;
+        case 'created':
+          response = await fetchCreatedMeetings(page);
+          break;
+        case 'ongoing':
+          response = await fetchAllMeetings(page);
+          break;
+        default:
+          return;
+      }
+
+      if (response) {
+        setMeetings((prevMeetings) => [
+          ...prevMeetings,
+          ...response.data.dataList,
+        ]); // 기존 모임 목록에 새로운 데이터를 추가
+        setTotalPages(response.data.pageInfo.totalPages);
+        setPage((prevPage) => prevPage + 1); // 다음 페이지로 이동
+      }
+    } catch (error) {
+      console.error('모임 목록 로드 중 오류 발생:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchAllMeetings = async (page: number): Promise<MeetingResponse> => {
     const response = await requestHeader.get(
       `/api/user/meeting/list?page=${page}&size=10`
     );
-    console.log('전체모임데이터', response.data.data.dataList);
-    return response.data.data.dataList;
+    return response.data;
   };
 
-  const fetchOngoingMeetings = async (page: number) => {
-    const allMeetings = await fetchAllMeetings(page);
-    const filteredOngoingMeetings = await allMeetings.data.data.dataList.filter(
-      (meeting: MettingListInfo) => meeting.meetingStatus === 'ONGOING'
-    );
-    console.log(filteredOngoingMeetings);
-  };
-
-  const fetchCreatedMeetings = async (page: number) => {
+  const fetchCreatedMeetings = async (
+    page: number
+  ): Promise<MeetingResponse> => {
     const response = await requestHeader.get(
       `/api/user/meeting/leader/list?page=${page}&size=10`
     );
-    console.log('내가만든 모임데이터', response.data.data.dataList);
-    return response.data.data.dataList;
+    return response.data;
   };
 
   return (
@@ -85,10 +114,17 @@ const MeetingHistory: React.FC<MyInfoProps> = ({ userInfo }) => {
       <div className="mymeeting-head">
         <h3>내 모임 내역</h3>
         <div className="filter">
-          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+          <select
+            value={filter}
+            onChange={(e) => {
+              setFilter(e.target.value);
+              setMeetings([]); // 모임 목록 초기화
+              setPage(1); // 페이지 초기화
+            }}
+          >
             <option value="all">전체</option>
-            <option value="ongoing">모집 중인 모임</option>
             <option value="created">내가 만든 모임</option>
+            <option value="ongoing">모집중인 모임</option>
           </select>
           <span className="xi-angle-down-thin dropdown-icon"></span>
         </div>
@@ -112,6 +148,8 @@ const MeetingHistory: React.FC<MyInfoProps> = ({ userInfo }) => {
           />
         ))}
       </div>
+      {loading && <div className="loading">로딩 중...</div>}{' '}
+      {/* 로딩 메시지 추가 */}
     </section>
   );
 };
