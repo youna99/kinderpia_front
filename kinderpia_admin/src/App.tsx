87e1c97447d67fac1a7f11edae068bd11f2f6cca
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-11-08 13:16:36 +0900

    feat : 관리자 로그인 복원

--- kinderpia_admin/src/App.tsx
+++ kinderpia_admin/src/App.tsx
@@ -5,9 +5,9 @@ import './App.css';
 import AdminLayout from './layout/AdminLayout';
 import Dashboard from './pages/Dashboard';
 import Members from './pages/Members';
-import Groups from './pages/Meetings';
 import Reports from './pages/Reports';
 import BadPersons from './pages/BadPersons';
+import Meetings from './pages/Meetings';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<AdminLayout />}>  {/* /admin 대신 / 사용 */}
             <Route index element={<Dashboard />} />
             <Route path="members" element={<Members />} />
-            <Route path="groups" element={<Groups />} />
+            <Route path="meetings" element={<Meetings />} />
             <Route path="reports" element={<Reports />} />
             <Route path="blacklist" element={<BadPersons />} />
           </Route>
