0583dd70dff0d995651ec0e07c6fc43d521e00d0
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-11-09 19:46:17 +0900

    feat : 한국 지도 추가, distinct 데이터 포맷 수정

---
+++ kinderpia_client/src/components/common/RegionMap.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import KoreaMap from '../../assets/KoreaMap';
+import SeoulMap from '../../assets/SeoulMap';
+
+import '../../styles/common/RegionMap.scss';
+
+interface RegionMapProps {
+  onDistrictClick?: (district: string) => void;
+  selectedDistrict?: string | null;
+}
+
+const RegionMap:React.FC<RegionMapProps> = ({
+  onDistrictClick,
+  selectedDistrict,
+}) => { 
+  const [hoveredDistrict, setHoveredDistrict] = useState<string | null>(null);
+  const [mapToggle, setMapToggle]=useState(true);
+
+  return (
+    <>
+      <div className="map-toggle">
+        <button 
+          className={mapToggle ? 'active' : ''}
+          onClick={() => setMapToggle(true)}
+        >
+          전국 지도
+        </button>
+        <button 
+          className={!mapToggle ? 'active' : ''}
+          onClick={() => setMapToggle(false)}
+        >
+          서울 지도
+        </button>
+      </div>
+      <hr/>
+      {mapToggle
+        ?
+          <KoreaMap
+            onRegionClick={onDistrictClick}
+            selectedRegion={selectedDistrict}
+            setMapToggle={setMapToggle} // 추가
+          />
+        :
+          <SeoulMap
+            onDistrictClick={onDistrictClick}
+            selectedDistrict={selectedDistrict}
+          />
+        }
+    </>
+  )
+}
+
+export default RegionMap
